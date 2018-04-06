65db2830bfceb45b120f4ace8fcbae5fd521c931
ryder <wqloveywh@gmail.com>
2018-04-06 16:22:03 +0800

    wangqiSoft
    修改demo的使用，删除Picasso 加载方式，修改部分报名

---
+++ app/src/main/java/com/wangqiSoftdemo/smartimageslider/Main2Activity.java
@@ -0,0 +1,54 @@
+package com.wangqiSoftdemo.smartimageslider;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wangqiSoft.smartimageslider.Animations.DescriptionAnimation;
+import com.wangqiSoft.smartimageslider.SliderLayout;
+import com.wangqiSoft.smartimageslider.SliderTypes.BaseSliderView;
+import com.wangqiSoft.smartimageslider.SliderTypes.DefaultSliderView;
+
+import java.util.HashMap;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private SliderLayout mSlider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mSlider = (SliderLayout) findViewById(R.id.slider);
+
+        HashMap<String,Integer> file_maps = new HashMap<>();
+        file_maps.put("轮播6",R.mipmap.a);
+        file_maps.put("轮播5",R.mipmap.b);
+        file_maps.put("轮播4",R.mipmap.c);
+        file_maps.put("轮播3", R.mipmap.d);
+        file_maps.put("轮播2", R.mipmap.e);
+
+        for(String name : file_maps.keySet()){
+            DefaultSliderView textSliderView = new DefaultSliderView(this);
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(file_maps.get(name))
+                    .empty(R.mipmap.ic_launcher)
+                    .setScaleType(BaseSliderView.ScaleType.Fit);
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            mSlider.addSlider(textSliderView);
+        }
+        mSlider.setPresetTransformer(SliderLayout.Transformer.RotateUp);
+        mSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mSlider.setCustomAnimation(new DescriptionAnimation());
+
+    }
+
+
+}
